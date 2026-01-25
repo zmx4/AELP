14a677793e1125bbc728df63bd185c3922a1d459
Tick Zhong <1601858426@qq.com>
2026-01-25 09:23:29 +0800

    测试

--- AELP/ViewModels/SummaryPageViewModel.cs
+++ AELP/ViewModels/SummaryPageViewModel.cs
@@ -1,9 +1,21 @@
-﻿namespace AELP.ViewModels;
+﻿using System.Collections.Generic;
+
+namespace AELP.ViewModels;
 
 public class SummaryPageViewModel : PageViewModel
 {
+    List<ProblemData> _problems;
     public SummaryPageViewModel()
     {
+        _problems = new List<ProblemData>();
         PageNames = Data.ApplicationPageNames.Summary;
+    }
+
+    public override void SetParameter(object parameter)
+    {
+        if (parameter is List<ProblemData> problems)
+        {
+            _problems = problems;
+        }
     }
 }
--- AELP/ViewModels/TestsPageViewModel.cs
+++ AELP/ViewModels/TestsPageViewModel.cs
@@ -1,9 +1,37 @@
-﻿namespace AELP.ViewModels;
+﻿using System.Collections.Generic;
+using AELP.Models;
+using AELP.Services;
+
+namespace AELP.ViewModels;
 
 public class TestsPageViewModel : PageViewModel
 {
-    public TestsPageViewModel()
+    private List<Word> _TestWords;
+    private List<ProblemData> _problemDatas;
+    
+    private readonly ITestDataStorageService _testDataStorageService;
+    private readonly IMistakeDataStorageService _mistakeDataStorageService;
+    private readonly ITestWordGetter _testWordGetter;
+    public TestsPageViewModel(ITestWordGetter testWordGetter,
+                              ITestDataStorageService testDataStorageService,
+                              IMistakeDataStorageService mistakeDataStorageService)
     {
+        _testWordGetter = testWordGetter;
+        _testDataStorageService = testDataStorageService;
+        _mistakeDataStorageService = mistakeDataStorageService;
         PageNames = Data.ApplicationPageNames.Tests;
     }
+
+
+
+
+    
+}
+
+public record ProblemData
+{
+    public string Word { get; init; }
+    public string Translation { get; init; }
+    public long CostTimeMs { get; init; }
+    public bool IsRight { get; init; }
 }
