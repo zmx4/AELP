a831413f914a9f9ee552b0ec21ee16a739ef5391
Tick Zhong <1601858426@qq.com>
2026-02-14 19:52:39 +0800

    通知服务

---
+++ AELP/Services/NotifyService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Controls.Notifications;
+
+namespace AELP.Services;
+
+public class NotifyService : INotifyService
+{
+    public async Task Notify(string title, string message, NotificationType type = NotificationType.Information)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Alert(string title, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+}
